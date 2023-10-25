eca87d4ebf692aa5869473d4a7966903491f3982
DillonPerry <116328477+DillonPerry@users.noreply.github.com>
2023-10-25 13:14:07 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -17,7 +17,7 @@ export enum Theme {
 }
 
 export const DEFAULT_CONFIG = {
-  submitKey: SubmitKey.CtrlEnter as SubmitKey,
+  submitKey: SubmitKey.Enter as SubmitKey,
   avatar: "1f603",
   fontSize: 14,
   theme: Theme.Auto as Theme,
@@ -27,10 +27,10 @@ export const DEFAULT_CONFIG = {
 
   disablePromptHint: false,
 
-  dontShowMaskSplashScreen: false, // dont show splash screen when create chat
+  dontShowMaskSplashScreen: true, // dont show splash screen when create chat
 
   modelConfig: {
-    model: "gpt-3.5-turbo" as ModelType,
+    model: "gpt-4" as ModelType,
     temperature: 0.5,
     max_tokens: 2000,
     presence_penalty: 0,
