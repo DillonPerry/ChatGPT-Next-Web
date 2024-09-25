1892642fe2969162ad51d00e3539b82e09c01460
DillonPerry <116328477+DillonPerry@users.noreply.github.com>
2024-09-25 10:41:21 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { getBuildConfig } from "./config/build";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "小浦ChatGPT私用版",
+  title: "梓清ChatGPT中转",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
@@ -18,7 +18,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "小浦ChatGPT私用版",
+    title: "梓清ChatGPT中转",
     statusBarStyle: "default",
   },
 };
